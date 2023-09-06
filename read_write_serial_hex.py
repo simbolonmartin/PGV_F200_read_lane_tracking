b0e6b8317a9258de78fae45e6ea5324588092c4f
Isak Martin Simbolon <simbolonmartin22@gmail.com>
2023-09-06 17:52:24 +0800

    Improve readibility by following pep8 format

--- read_write_serial_hex.py
+++ read_write_serial_hex.py
@@ -1,20 +1,29 @@
-import serial
 import time
+import serial
 
-class PGVCommunication():
-       
+
+class PGVCommunication:
     def __init__(self) -> None:
         # self.usb_port = "/dev/ttyUSB1"
-        self.usb_port = "/dev/pgv_sensor"
+        # self.usb_port = "COM3" #for Windows
+        self.usb_port = "/dev/pgv_sensor"  # for Ubuntu
         self.baud_rate = 115200
+        self.result_write = None
+        self.result_read = None
+        self.number_of_lanes = None
+        self.angle_value = None
+        self.y_position = None
+        self.tracking_result = None
         self.check_connection()
         self.initialize_message()
         self.trigger_sensor()
 
     def check_connection(self):
         try:
-            self.serial_channel = serial.Serial(self.usb_port, self.baud_rate, timeout=3, parity=serial.PARITY_EVEN)
-            print("Serial details params: ", self.serial_channel)
+            self.serial_channel = serial.Serial(
+                self.usb_port, self.baud_rate, timeout=3, parity=serial.PARITY_EVEN
+            )
+            # print("Serial details params: ", self.serial_channel)
         except:
             print("Couldn't connect to usb port")
 
@@ -43,18 +52,17 @@ class PGVCommunication():
         # print(f'hex access index 1: {self.result_read[1]}')
         # print(f'hex access index -1: {self.result_read[-1]}')
         # print(f'length: {len(self.result_read_hex)}')
-    
+
     def print_result_read(self):
         counter = 1
         for _ in self.result_read:
-            print(f'{counter}  = {_}')
+            print(f"{counter}  = {_}")
             if counter == 2:
                 print(f"binary representation: {bin(_)[2:].zfill(8)}")
             counter += 1
 
     def calculate(self):
-        """Calculate the angle value, y_position, and number of lanes detected.
-        """
+        """Calculate the angle value, y_position, and number of lanes detected."""
         multiplier = 0x80
         byte_2_binary = bin(self.result_read[1])[2:].zfill(8)
         # print(f"len binary =  {len(byte_2_binary)}")
@@ -63,8 +71,8 @@ class PGVCommunication():
         self.number_of_lanes = int(byte_2_binary[2]) * 2 + int(byte_2_binary[3])
         # print(f'number_of_lanes: {self.number_of_lanes}')
         self.angle_value = self.result_read[10] * multiplier + self.result_read[11]
-        if self.angle_value > 180 :
-            self.angle_value = - (360-self.angle_value)
+        if self.angle_value > 180:
+            self.angle_value = -(360 - self.angle_value)
         y_position_unsigned = self.result_read[6] * multiplier + self.result_read[7]
         if y_position_unsigned > 0x2000:
             self.y_position = -1 * int(0x4000 - y_position_unsigned)
@@ -72,8 +80,9 @@ class PGVCommunication():
             self.y_position = int(y_position_unsigned)
         self.tracking_result = [self.number_of_lanes, self.angle_value, self.y_position]
         # print(self.tracking_result)
-    
+
     def trigger_sensor(self):
+        """The sensor needs to be triggered first by sending color request and follow_left_lane message"""
         try:
             self.send_message(self.request_blue)
             time.sleep(0.1)
@@ -88,28 +97,25 @@ class PGVCommunication():
             time.sleep(1)
 
     def stream_value(self):
-        """View all the tracking lane values information (angle_value, y_position, number_of_lanes)
-        """
+        """View all the tracking lane values information (angle_value, y_position, number_of_lanes)"""
         try:
             # self.send_message(self.request_blue)
             # time.sleep(0.1)
             # self.send_message(self.follow_left_lane)
             # time.sleep(3)
             while True:
-                self.send_message(self.position_value)
-                self.read_message()
-                self.calculate()
-                # self.print_result_read()
-                # print(f'angle_value: {self.angle_value} \t y_position: {self.y_position} \t number_of_lanes: {self.number_of_lanes}') #TODO: comment this on deployment
-                time.sleep(0.1) #TODO: check this if it is enough to have a good movement result
+                self.update_value()
                 if __name__ == "__main__":
-                    print(f'number_of_lanes: {self.number_of_lanes} \t angle_value: {self.angle_value} \t y_position: {self.y_position}')
+                    print(
+                        f"number_of_lanes: {self.number_of_lanes} \t angle_value: {self.angle_value} \t y_position: {self.y_position}"
+                    )
         except Exception as error:
-            print (error)
+            print(error)
         finally:
             self.serial_channel.close()
 
     def update_value(self):
+        """Will calculate the tracking result with delay 0.1 for each call"""
         try:
             self.send_message(self.position_value)
             self.read_message()
@@ -117,8 +123,8 @@ class PGVCommunication():
             time.sleep(0.1)
         except Exception as error:
             print(error)
-        
+
+
 if __name__ == "__main__":
     PGVCommunicationObject = PGVCommunication()
     PGVCommunicationObject.stream_value()
-    
